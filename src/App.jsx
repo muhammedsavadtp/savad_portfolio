0106780c46eb019223227e24f5ca12525cd2c8d7
muhammed <www.savad34@gmail.com>
2023-03-28 18:09:45 +0530

    remove: hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import React,{ useState } from 'react'
 import Home from './components/home/Home'
-import { Route,Routes } from 'react-router-dom'
 function App() {
 
   return (
     <>
-      <Routes>
-        <Route path='/' element={ <Home/>} />
-      </Routes>
-     
+      
+      <Home/>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import App from './App'
 import './index.css'
 import { store } from './store/store'
 import { Provider } from 'react-redux'
-import { HashRouter } from 'react-router-dom'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <Provider store={store}>
-    <HashRouter>
-      <App /> 
-    </HashRouter>
+   
+      <App />
+    
     </Provider>,
  
 )
